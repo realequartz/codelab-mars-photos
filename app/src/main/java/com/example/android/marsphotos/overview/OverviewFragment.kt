07db0df86789637600a93ecc696120e6964db233
realequartz <eduardos.silvaalves@gmail.com>
2021-10-02 19:57:01 -0300

    Add loading and error images

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.android.marsphotos.databinding.FragmentOverviewBinding
 import com.example.android.marsphotos.databinding.GridViewItemBinding
 
 /**
